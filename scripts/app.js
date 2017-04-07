55441d34e0581a0ad15badc404b068639d9ec732
Zeke Sikelianos <zeke@sikelianos.com>
2017-04-07 16:18:55 -0700

    fix javascript error

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,8 @@ function detectLocation () {
   $.getJSON('http://ipinfo.io', function (ipinfo) {
     var fields = ['city', 'state', 'zip', 'ip', 'loc']
     fields.forEach(function (field) {
-      document.querySelector('#contact input[name=' + field ']').value = ipinfo[field]
+      var input = document.querySelector('#contact input[name=' + field + ']')
+      input.value = ipinfo[field]
     })
   })
 }
